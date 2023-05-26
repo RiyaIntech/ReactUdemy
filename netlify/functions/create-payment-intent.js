80bfbb4bde1ef312216511c7f78a696a22172024
riya.sankesara <riya.sankesra@ics-global.in>
2023-05-26 17:28:20 +0530

    stripe code commited

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -3,10 +3,10 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event) => {
   try {
-    const { amount } = JSON.parse(event.body);
-
+    const { amount,description } = JSON.parse(event.body);
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
+      description,
       currency: "usd",
       payment_method_types: ["card"],
     });
